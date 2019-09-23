70179a70f84b619092cb4c75dfa6686525d14da3
alihaider334 <ali334.haider@gmail.com>
2019-09-23 15:16:27 -0400

    App Login/Register Activities Designed

---
+++ KidsFly/app/src/main/java/com/ali/kidsfly/AppLauncherActivity.kt
@@ -0,0 +1,22 @@
+package com.ali.kidsfly
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_app_launcher.*
+
+class AppLauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_launcher)
+
+        register.setOnClickListener {
+            val intent= Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
